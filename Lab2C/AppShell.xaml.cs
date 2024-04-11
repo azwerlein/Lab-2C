71edca031ad10123f9f9c3a8a1d7e66d193c6d85
Burning Cactus <azwerlein77@gmail.com>
2024-04-10 20:46:06 -0500

    Completed lab 2C

---
+++ Lab2C/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab2C;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Lab2C/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using AwsomeApp;
+
+namespace Lab2C;
+
+public partial class MainPage : ContentPage
+{
+    private bool _spaceOnePressed = false;
+    private bool _spaceTwoPressed = false;
+    private string _wordOne = "";
+    private string _wordTwo = "";
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnDotOneClicked(object sender, EventArgs e)
+    {
+        SelectedCharacterOne.Text = _wordOne + ".";
+        _spaceOnePressed = false;
+    }
+
+    private void OnDashOneClicked(object sender, EventArgs e)
+    {
+        SelectedCharacterOne.Text = _wordOne + "-";
+        _spaceOnePressed = false;
+    }
+
+    private void OnSpaceOneClicked(object sender, EventArgs e)
+    {
+        if (_spaceOnePressed)
+        {
+            char text = Morse.MorseCoder(SelectedCharacterOne.Text);
+            OutputOne.Text += text;
+            _spaceOnePressed = false;
+            SelectedCharacterOne.Text = _wordOne = "";
+        }
+        else
+        {
+            _wordOne = SelectedCharacterOne.Text;
+            _spaceOnePressed = true;
+        }
+    }
+    
+    private void OnDotTwoClicked(object sender, EventArgs e)
+    {
+        SelectedCharacterTwo.Text = _wordTwo + ".";
+        _spaceTwoPressed = false;
+    }
+
+    private void OnDashTwoClicked(object sender, EventArgs e)
+    {
+        SelectedCharacterTwo.Text = _wordTwo + "-";
+        _spaceTwoPressed = false;
+    }
+
+    private void OnSpaceTwoClicked(object sender, EventArgs e)
+    {
+        if (_spaceTwoPressed)
+        {
+            char text = Morse.MorseCoder(SelectedCharacterTwo.Text);
+            OutputTwo.Text += text;
+            _spaceTwoPressed = false;
+            SelectedCharacterTwo.Text = _wordTwo = "";
+        }
+        else
+        {
+            _wordTwo = SelectedCharacterTwo.Text;
+            _spaceTwoPressed = true;
+        }
+    }
+}
